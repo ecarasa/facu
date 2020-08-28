ff27f0f191907653c0ac8a4c62892527d64b11c6
Emiliano Carasa <emilianocarasa@gmail.com>
2020-08-28 14:10:55 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b81b332fa9c0597304b67d0e65127394'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '582a3608-aff8-4069-a7bb-5ebaace6d5ae'
+};
